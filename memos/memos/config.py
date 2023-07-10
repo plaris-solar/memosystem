f6516394d5212268ce8810d4e49de30900a4b6ee
José Antonio Becerra <jbecerra@completesolar.com>
2023-07-10 14:02:44 -0600

    url fix

--- memos/memos/config.py
+++ memos/memos/config.py
@@ -1,7 +1,7 @@
 import os
 
 class Config:
-    SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://memosystem:memopw@mysql/memos'
+    SQLALCHEMY_DATABASE_URI = os.environ.get('SQLALCHEMY_DATABASE_URI')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     SQLALCHEMY_ENGINE_OPTIONS = {
         'pool_pre_ping': True,
